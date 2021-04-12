babfa1ee68c25171059f66f33490e0573d9e81f8
LeAiMy <hantctk42@gmail.com>
2021-04-12 22:12:35 +0700

    Nhiem vu cua Ha da hoàn thanh xuat sac 🕙🕙🕙🕙🎉🎉

---
+++ DanhSachSinhVien.sql
@@ -0,0 +1,28 @@
+﻿CREATE DATABASE QL_DanhSachSinhVien
+GO
+USE QL_DanhSachSinhVien
+GO
+
+CREATE TABLE Student
+(
+	MSSV VARCHAR(7) PRIMARY KEY,
+	HoDem NVARCHAR(50),
+	Ten NVARCHAR(10),
+)
+GO
+
+INSERT INTO Student(MSSV,HoDem,Ten) 
+VALUES 
+	('1812751',N'Nguyễn Thị',N'Hà'),
+	('1812750',N'Lệ Ái',N'Mỹ'),
+	('1812756',N'Nguyễn Trọng',N'Bóng')
+SELECT * FROM Student
+GO
+
+ CREATE PROC _getAllStudent
+ AS
+	 SELECT * FROM Student
+
+GO
+
+EXEC _getAllStudent
