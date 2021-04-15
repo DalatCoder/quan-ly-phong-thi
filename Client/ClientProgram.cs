23aa6ef862305ecd640bea57b77816290be2f0de
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2021-04-15 21:44:53 +0700

    Bổ sung thông báo phía giao diện máy con 🐱‍👤🐱‍👤🐱‍👤

--- Client/ClientProgram.cs
+++ Client/ClientProgram.cs
@@ -15,79 +15,62 @@ namespace Client
 {
 	public class ClientProgram
 	{
+		private const int BUFFER_SIZE = 1 * 1024 * 1024 * 20; // 20Mb
+
 		IPEndPoint IP;
 		Socket server;
 
 		string savePath = null; // Thu muc luu de thi
 
+		#region Define events
+
 		event Action<string> _onSuccessNotification;
 		public event Action<string> OnSuccessNotification
 		{
-			add
-			{
-				_onSuccessNotification += value;
-			}
-			remove
-			{
-				_onSuccessNotification -= value;
-			}
+			add { _onSuccessNotification += value; }
+			remove { _onSuccessNotification -= value; }
 		}
 
 		event Action<string, Exception> _onErrorNotification;
 		public event Action<string, Exception> OnErrorNotification
 		{
-			add
-			{
-				_onErrorNotification += value;
-			}
-			remove
-			{
-				_onErrorNotification -= value;
-			}
+			add { _onErrorNotification += value; }
+			remove { _onErrorNotification -= value; }
+		}
+
+		event Action _onClientDisconnected;
+		public event Action OnClientDisconnected
+		{
+			add { _onClientDisconnected += value; }
+			remove { _onClientDisconnected -= value; }
 		}
 
 		event Action<string> _onReceivedExam;
 		public event Action<string> OnReceivedExam
 		{
-			add
-			{
-				_onReceivedExam += value;
-			}
-			remove
-			{
-				_onReceivedExam -= value;
-			}
+			add { _onReceivedExam += value; }
+			remove { _onReceivedExam -= value; }
 		}
 
 		event Action<List<string>> _onCamChuongTrinh;
 		public event Action<List<string>> OnCamChuongTrinh
 		{
-			add
-			{
-				_onCamChuongTrinh += value;
-			}
-			remove
-			{
-				_onCamChuongTrinh -= value;
-			}
+			add { _onCamChuongTrinh += value; }
+			remove { _onCamChuongTrinh -= value; }
 		}
 
 		event Action<SubjectAndTime> _onNhanMonThiVaThoiGian;
 		public event Action<SubjectAndTime> OnNhanMonThiVaThoiGian
 		{
-			add
-			{
-				_onNhanMonThiVaThoiGian += value;
-			}
-			remove
-			{
-				_onNhanMonThiVaThoiGian -= value;
-			}
+			add { _onNhanMonThiVaThoiGian += value; }
+			remove { _onNhanMonThiVaThoiGian -= value; }
 		}
 
 		public Action<string> onNhanThongBao;
 		public Action<List<Student>> onNhanDanhSachSVTuExcel;
 		public Action<int> onNhanSoPhut;
+
+		#endregion
 
 		public void Connect(string hostname, int port)
 		{
@@ -113,6 +96,9 @@ namespace Client
 			{
 				if (_onErrorNotification != null)
 					_onErrorNotification("Có lỗi xảy ra trong quá trình kết nối đến máy chủ", ex);
+
+				if (_onClientDisconnected != null)
+					_onClientDisconnected();
 			}
 		}
 
@@ -145,9 +131,11 @@ namespace Client
 
 		public void SendStudent(Student student)
 		{
-			DataContainer container = new DataContainer(DataContainerType.GuiSinhVien,student);
-
+			DataContainer container = new DataContainer(DataContainerType.GuiSinhVien, student);
 			SendDataToServer(container);
+
+			if (_onSuccessNotification != null)
+				_onSuccessNotification("Đã gửi thông tin sinh viên lên máy chủ");
 		}
 
 		void Receive()
@@ -156,7 +144,7 @@ namespace Client
 			{
 				while (true)
 				{
-					byte[] buffer = new byte[1024 * 1024 * 20];
+					byte[] buffer = new byte[BUFFER_SIZE];
 					server.Receive(buffer);
 
 					DataContainer dataContainer = DataContainer.Deserialize(buffer);
@@ -164,7 +152,6 @@ namespace Client
 					switch (dataContainer.Type)
 					{
 						case DataContainerType.PhatDe:
-
 							FileContainer fileContainer = dataContainer.Data as FileContainer;
 
 							string savePath = fileContainer.SavePath;
@@ -243,7 +230,7 @@ namespace Client
 							if (string.IsNullOrWhiteSpace(fileNopBai))
 							{
 								if (_onErrorNotification != null)
-									_onErrorNotification("Không tìm thấy thư mục lưu bài", null);
+									_onErrorNotification("Không tìm thấy file nén bài làm", null);
 
 								break;
 							}
@@ -255,10 +242,12 @@ namespace Client
 
 							SendDataToServer(dataContainerNopBai);
 
+							if (_onSuccessNotification != null)
+								_onSuccessNotification("Đã gửi file nén bài làm lên máy chủ");
+
 							break;
 
 						case DataContainerType.CamChuongTrinh:
-
 							List<string> programs = dataContainer.Data as List<string>;
 
 							if (_onCamChuongTrinh != null)
@@ -267,22 +256,27 @@ namespace Client
 							break;
 
 						case DataContainerType.GuiThongBaoAll:
-
 							string message = dataContainer.Data.ToString();
 
-							onNhanThongBao(message);
+							if (onNhanThongBao != null)
+								onNhanThongBao(message);
 
 							break;
 
 						case DataContainerType.GuiDanhSachSV:
-
 							List<Student> students = dataContainer.Data as List<Student>;
-							onNhanDanhSachSVTuExcel(students);
+
+							if (onNhanDanhSachSVTuExcel != null)
+								onNhanDanhSachSVTuExcel(students);
+
 							break;
 
 						case DataContainerType.BatDauLamBai:
 							int minnute = Convert.ToInt32(dataContainer.Data);
-							onNhanSoPhut(minnute);
+
+							if (onNhanSoPhut != null)
+								onNhanSoPhut(minnute);
+
 							break;
 
 						case DataContainerType.GuiThoiGianLamBai:
@@ -294,36 +288,23 @@ namespace Client
 
 							break;
 
-						case DataContainerType.SendList:
-							break;
-						case DataContainerType.SendStudent:
-							break;
-						case DataContainerType.SendString:
-							break;
-						case DataContainerType.SendPcName:
-							break;
-
 						case DataContainerType.DisconnectAll:
 
 							if (_onSuccessNotification != null)
 								_onSuccessNotification("Đã ngắt đường truyền do nhận được yêu cầu đóng kết nối từ máy chủ");
 
 							CloseConnection();
-							break;
 
-						case DataContainerType.BeginExam:
-
-							break;
-
-						case DataContainerType.FinishExam:
+							if (_onClientDisconnected != null)
+								_onClientDisconnected();
 
 							break;
 
-						case DataContainerType.LockClient:
-							break;
-						case DataContainerType.Undefined:
-							break;
 						default:
+
+							if (_onErrorNotification != null)
+								_onErrorNotification("Lỗi không xác định, vui lòng khởi động lại phần mềm", null);
+
 							break;
 					}
 				}
@@ -337,6 +318,9 @@ namespace Client
 				}
 
 				CloseConnection();
+
+				if (_onClientDisconnected != null)
+					_onClientDisconnected();
 			}
 		}
 	}
